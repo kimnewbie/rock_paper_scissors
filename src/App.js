fa461c4c0d109023908f97c5bb8fb0594753ed87
youjin kim <gracecancho@gmail.com>
2023-10-10 13:08:28 +0900

    rps

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RPS from './components/RPS';
+import Test from './components/Test';
+
+function App() {
+  return (
+    // <RPS />
+    <Test />
+  );
+}
+
+export default App;
